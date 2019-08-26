1393712c6a3b4924b2f89606a8696fbf44147e22
Sarah Thompson <sethompson@upei.ca>
2019-08-26 15:23:53 -0300

    created barebones calendar layout without dates listed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Day(props) {
+    return <div className="day">{props.name}</div>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Week(props) {
+
+    return (
+        <div className="week">
+            <Day name="Monday"></Day>
+            <Day name="Tuesday"></Day>
+            <Day name="Wednesday"></Day>
+            <Day name="Thusday"></Day>
+            <Day name="Friday"></Day>
+            <Day name="Saturday"></Day>
+            <Day name="Sunday"></Day>
+        </div>
+    );
+}
+
+function Month(props) {
+    
+
+    return (
+        <div className="month">
+            <Week></Week>
+            <Week></Week>
+            <Week></Week>
+            <Week></Week>
+            <Week></Week>
+        </div>
+    );
+}
+
+class Calendar extends React.Component{
+
+    render(){
+        return (
+            <div className="calendar">
+                <Month></Month>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Calendar />,
+    document.getElementById('root')
+  );
+  
