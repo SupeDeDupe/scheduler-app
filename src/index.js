de0f56a3484873f64ca68045c1b28275127cc149
Sarah Thompson <sethompson@upei.ca>
2019-08-27 17:37:19 -0300

    added the date numbers to the calendar

--- src/index.js
+++ src/index.js
@@ -7,57 +7,73 @@ import Row from 'react-bootstrap/Row';
 
 function Day(props) {
     return (
-        <button className="day"></button>
+        <button className="day">{props.date ? props.date : null}</button>
     );
 }
 
 function Week(props) {
+    console.log(props.date);
+    let date = props.date;
+    let tempDate = date;
+    const offset = props.offset ? props.offset : 0;
+    
+    let week = offset ? new Array(7 - offset) : new Array(7);
+    week = week.fill(null).map(() => tempDate++);
+    console.log(week.toString());
+
+    let id=0;
+    let blankDays = new Array(offset).fill(<td key={id++}><Day></Day></td>);
+
+    let days = week.map((index) => {
+        return (
+            <td key={index+offset}><Day date={date <= props.endDate ? date++ : null}></Day></td>
+        );
+    });
 
     return (
         <tr className="week">
-            <td><Day name="Monday"></Day></td>
-            <td><Day name="Tuesday"></Day></td>
-            <td><Day name="Wednesday"></Day></td>
-            <td><Day name="Thusday"></Day></td>
-            <td><Day name="Friday"></Day></td>
-            <td><Day name="Saturday"></Day></td>
-            <td><Day name="Sunday"></Day></td>
+            {blankDays}
+            {days}
         </tr>
     );
 }
 
 function Month(props) {
-    let currentMonth = (props.date).getMonth();
-    let monthName = getMonthName(currentMonth);
+    let month = (props.date).getMonth();
+    let monthName = getMonthName(month);
+    let year = ((props.date).getFullYear()).toString();
     
-    let monthYear = monthName + " " + ((props.date).getFullYear()).toString();
-    // let date = new Date(2019, 8).getDay();
-    // let date2 = 32 — new Date(2019, 8, 32).getDate();
+    
+    let monthAndYear = monthName + " " + year;
+    let offset = props.date.getDay();
+    let date = 1;
+    let endDate = getMonthTotal(year, month);
+    const sevenDays = 7;
     return (
         <div className="month">
             <Row md="auto" className="justify-content-md-center">
                 <Button id="prev" variant="outline-secondary">Prev</Button>
-                <h2>{monthYear}</h2>
+                <h2>{monthAndYear}</h2>
                 <Button id="next" variant="outline-secondary">Next</Button>
             </Row>
             <table>
                 <thead>
                     <tr>
+                        <th>Sun</th>
                         <th>Mon</th>
                         <th>Tue</th>
                         <th>Wed</th>
                         <th>Thu</th>
                         <th>Fri</th>
                         <th>Sat</th>
-                        <th>Sun</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <Week></Week>
-                    <Week></Week>
-                    <Week></Week>
-                    <Week></Week>
-                    <Week></Week>
+                    <Week offset={offset} date={date} endDate={endDate}></Week>
+                    <Week date={date+=sevenDays-offset} endDate={endDate}></Week>
+                    <Week date={date+=sevenDays} endDate={endDate}></Week>
+                    <Week date={date+=sevenDays} endDate={endDate}></Week>
+                    <Week date={date+=sevenDays} endDate={endDate}></Week>
                 </tbody>
             </table>
         </div>
@@ -65,7 +81,7 @@ function Month(props) {
 }
 
 function Calendar(props) {
-    let date = new Date();
+    let date = new Date(2019, 7);
 
     return (
         <Container className="calendar">
@@ -84,5 +100,9 @@ function getMonthName (num) {
     var months = [ "January", "February", "March", "April", "May", "June", 
                 "July", "August", "September", "October", "November", "December" ];
     return months[num];
+}
+
+function getMonthTotal (year, month){
+    return 32 - new Date(year, month, 32).getDate();
 }
   
